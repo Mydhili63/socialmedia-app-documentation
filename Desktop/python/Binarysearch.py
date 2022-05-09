db47014c092c41ebb04d13596e7cb2b2c372e298
mydhili63 <y.mydhili63@gmail.com>
2022-05-09 22:08:46 +0530

    Binarysearch

---
+++ Desktop/python/Binarysearch.py
@@ -0,0 +1,28 @@
+def binarysearch(list1, key):
+     low = 0
+     high = len(list1)-1
+     mid = 0
+     Found = False  
+     while low<=high and not Found :
+        mid=(low + high) // 2   
+        if key == list1[mid]:
+            Found = True
+        elif key > list1[mid]:
+            low = mid+1
+        else:
+            high = mid-1
+     if Found == True:
+        print("key is found")
+     else:
+        print("key is not found")
+
+list1 =[]
+n=int(input("Enter number of elements :"))
+print("Enter the list of elements:")
+for i in range(0,n):
+    m=int(input())
+    list1.append(m)
+list1.sort()
+print(list1)
+key = int(input("enter the key :"))
+binarysearch(list1,key)
