e1bd1e95d2fae12c033000106f35718173b5b316
mydhili63 <y.mydhili63@gmail.com>
2022-06-24 16:49:46 +0530

    nodejs

--- Desktop/nodejs/m.js
+++
@@ -1,8 +0,0 @@
-//creating a server
-const http = require('http');
-const server = http.createServer((req,res) => {
-    console.log('request made');
-});
-server.listen(300,'localhost', () =>{
-    console.log('listening for requests on port 300');
-});
